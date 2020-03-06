3441dfc412a1aeb02eacf97a14f2454a5432cabb
Josh <34256170+ScienceOfSleep@users.noreply.github.com>
2020-03-06 16:33:13 -0700

    Header and Footer

--- src/components/header.js
+++ src/components/header.js
@@ -44,25 +44,35 @@ const Header = () => {
 
 
     const StyledLink = styled(Link)`
-      color: #ffffff;
+      display: block;
       text-decoration: none;
+      color: #000000;
+      background-color: #ffffff;
+      &.currentPage{
+        border-bottom: 2px solid #000000;
+      }
+      @media only screen and (min-width: 1120px) {
+      color: #ffffff;
       &[data-active='true']{
         color: #000000;
-      }
-  
+      }  
       &.currentPage{
       border-bottom: 2px solid #ffffff;
         &[data-active='true']{
         border-bottom: 2px solid #000000;
         }
       }
+      }
     `;
 
 
     const ListLink = props => (
         <li css={css`
+              display: block;
+              @media only screen and (min-width: 1120px) {
               display: inline-block;
               margin-left: 1rem;
+              }
             `}>
             <h5>
                 <StyledLink
@@ -80,10 +90,11 @@ const Header = () => {
         <header
             data-active={scrolled}
             css={css`
+              grid-area: navbar;
               background-color: transparent;
               z-index: 1;
               width: 100%;
-              grid-area: navbar;
+              
               &[data-active='true']{
                 background-color: #ffffff;
                 position: fixed;
@@ -138,26 +149,102 @@ const Header = () => {
                     >
                         <Link to="/">
                             <Img fluid={data.blacklogo.childImageSharp.fluid}/>
-                        </Link></div>
+                        </Link>
+                    </div>
                 </div>
-                <nav css={css`
-                  display: flex;
-                  justify-content: flex-start;
-                  align-self: end;
-                  padding-left: 1rem;
-                  padding-bottom: 1rem;
-                    @media only screen and (min-width: 40.063em) {
-                      font-size: 1.5rem;
-                      align-self: center;
-                      margin-top: 1rem;
-                    }
-                    @media only screen and (min-width: 64.063em) {
-                      justify-content: flex-end;
-                      font-size: inherit;
-                    }
-                `}
+                <nav
+                    css={css`
+                          display: flex;
+                          justify-content: flex-start;
+                          align-self: end;
+                          padding-left: 1rem;
+                          padding-bottom: 1rem;
+                            @media only screen and (min-width: 40.063em) {
+                              font-size: 1.5rem;
+                              align-self: center;
+                              margin-top: 1rem;
+                            }
+                            @media only screen and (min-width: 64.063em) {
+                              justify-content: flex-end;
+                              font-size: inherit;
+                            }
+                        `}
                 >
-                    <ul css={css`padding-left: 0;`}>
+                    <a
+                        href="#main-menu"
+                        id="main-menu-toggle"
+                        className="menu-toggle"
+                        aria-label="Open main menu"
+                        data-active={scrolled}
+                        css={css`
+                              color: #ffffff;
+                              padding-top: 1rem;
+                              padding-right: 1rem;
+                              &[data-active='true']{
+                                color: #000000;
+                              }
+                              @media only screen and (min-width: 1120px) {
+                              display: none;
+                              }
+                            `}
+                    >
+                        <h4
+                            css={css`
+                              font-weight: bold;
+                            `}
+                        >
+                            Menu
+                        </h4>
+                    </a>
+
+                    <ul
+                        id="main-menu"
+                        css={css`
+                                  @media only screen and (max-width: 1120px) {
+                                    position: fixed;
+                                    display: none;
+                                    right: -200px;
+                                    top: -4px;
+                                    height: auto;
+                                    overflow-y: auto;
+                                    overflow-x: visible;
+                                    transition: right .3s ease;
+                                    z-index: 999;
+                                    background-color: #ffffff;
+                                    padding-bottom: 1rem;
+                                    padding-right: 1rem;
+                                    border: 4px solid var(--brand-color);
+                                    border-radius: .25rem;
+                                    &:target {
+                                      display: block;
+                                      right: -4px;
+                                      outline: none;
+                                      animation: moveDown 0.5s ease-in-out;
+                                      + .backdrop {
+                                        position: fixed;
+                                        display: block;  
+                                        left: 0;
+                                        top: 0;
+                                        width: 100vw;
+                                        height: 100vh;
+                                        z-index: 998;
+                                        background: rgba(0,0,0,.85);
+                                        cursor: default;
+                                      }
+                                    }
+                                    @keyframes moveDown {
+                                      from {
+                                        transform: translateY(-50rem);
+                                      }
+                                      to {
+                                        transform: translateY(0rem);
+                                      }
+                                    }
+                              @media only screen and (min-width: 1120px) {
+                                padding-left: 0;
+                              }
+                            `}
+                    >
                         <ListLink to="/" data-active={scrolled}>Home</ListLink>
                         <ListLink to="/features">Features</ListLink>
                         <ListLink to="/pastcustomers">Past Customers</ListLink>
@@ -165,7 +252,43 @@ const Header = () => {
                         <ListLink to="/blog">Blog</ListLink>
                         <ListLink to="/cart">Cart</ListLink>
                         <ListLink to="/contact">Contact</ListLink>
+                        <li
+                            css={css`
+                              list-style: none;
+                              @media only screen and (min-width: 1120px) {
+                                display: none;
+                              }
+                            `}
+                        >
+                            <h5>
+                                <a
+                                    href="#main-menu-toggle"
+                                    id="main-menu-close"
+                                    className="menu-close"
+                                    aria-label="Close main menu"
+                                    css={css`
+                                  color: #000000;
+                                  background-color: #ffffff;
+                                  text-decoration: none;
+                                  display: block;
+                                  @media only screen and (min-width: 1120px) {
+                                    display: none;
+                                  }
+                                `}
+                                >
+                                    Close
+                                </a>
+                            </h5>
+                        </li>
+
                     </ul>
+                    <a
+                        href="#main-menu-toggle"
+                        className="backdrop"
+                        tabIndex="-1"
+                        hidden
+                    >
+                    </a>
                 </nav>
             </section>
         </header>
